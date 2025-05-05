4021351ac8398b9f716bbb858d0c6275d1565a75
Eduardo Sodré <arieldreis@gmail.com>
2025-05-05 15:59:30 -0300

    Create ex008

---
+++ ex008.cs
@@ -0,0 +1,46 @@
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            double Salario;
+
+            Console.WriteLine("Informe o seu salário");
+            Console.WriteLine();
+            Console.Write("Salário: ");
+            double.TryParse(Console.ReadLine(), out Salario);
+            Console.WriteLine();
+
+            Console.Write("Salário atual: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(Salario.ToString());
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("(-)");
+
+            Console.Write("Novo salário: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(p.CalculaSalario(Salario));
+            Console.ReadKey();
+        }
+
+        private double CalculaSalario(double Salario)
+        {
+            double NovoSalario = 0;
+
+            if (Salario >= 1700)
+            {
+                NovoSalario = (Salario + 200);
+            }
+            else
+            {
+                NovoSalario = (Salario + 300);
+            }
+
+            return NovoSalario;
+        }
+    }
+}
