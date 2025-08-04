60f70c2ff1167a090f7a49900aacd9a1965bfc4e
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-08-04 15:22:08 -0300

    Interface graphic

---
+++ form1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            linkLabel1 = new LinkLabel();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(10, 10);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Monotype Corsiva", 30F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Pink;
+            label1.Location = new Point(228, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(347, 49);
+            label1.TabIndex = 1;
+            label1.Text = "SPAR AEROFLART";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Rounded MT Bold", 16F, FontStyle.Italic);
+            label2.ForeColor = Color.Pink;
+            label2.Location = new Point(228, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(326, 26);
+            label2.TabIndex = 2;
+            label2.Text = "Email ou Nome de usuário(a):";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Pink;
+            label3.Location = new Point(228, 169);
+            label3.Name = "label3";
+            label3.Size = new Size(81, 24);
+            label3.TabIndex = 3;
+            label3.Text = "Senha:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(228, 133);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(326, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(228, 212);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(326, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel1.LinkColor = Color.Pink;
+            linkLabel1.Location = new Point(311, 269);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(161, 22);
+            linkLabel1.TabIndex = 6;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Esqueci á Senha";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Fuchsia;
+            button1.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(343, 316);
+            button1.Name = "button1";
+            button1.Size = new Size(93, 35);
+            button1.TabIndex = 7;
+            button1.Text = "Entrar";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Purple;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(linkLabel1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(flowLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private LinkLabel linkLabel1;
+        private Button button1;
+    }
+}
