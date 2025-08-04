08457bcd08c800ed8d3ab27e193094a3e2a83969
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-08-04 16:05:53 -0300

    Interface 2

---
+++ form2/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace form2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            textBox4 = new TextBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(60, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 24);
+            label1.TabIndex = 0;
+            label1.Text = "ID";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(107, 65);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(318, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(60, 111);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 24);
+            label2.TabIndex = 2;
+            label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(60, 155);
+            label3.Name = "label3";
+            label3.Size = new Size(108, 24);
+            label3.TabIndex = 3;
+            label3.Text = "Endereço";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(60, 195);
+            label4.Name = "label4";
+            label4.Size = new Size(98, 24);
+            label4.TabIndex = 4;
+            label4.Text = "Telefone";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(60, 241);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 24);
+            label5.TabIndex = 5;
+            label5.Text = "Email";
+            label5.Click += label5_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(154, 111);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(271, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(187, 155);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(238, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(175, 195);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(250, 24);
+            richTextBox1.TabIndex = 8;
+            richTextBox1.Text = "";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(143, 242);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(282, 23);
+            textBox4.TabIndex = 9;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(350, 316);
+            button2.Name = "button2";
+            button2.Size = new Size(114, 35);
+            button2.TabIndex = 11;
+            button2.Text = "Sair";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.MenuHighlight;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(textBox4);
+            Controls.Add(richTextBox1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private RichTextBox richTextBox1;
+        private TextBox textBox4;
+        private Button button2;
+    }
+}
---
+++ form2/Form1.cs
@@ -0,0 +1,25 @@
+namespace form2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
