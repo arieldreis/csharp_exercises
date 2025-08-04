b70891913fd12023e226be221fd11e68b6de5e1b
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-08-04 16:24:34 -0300

    Thrid interface graphic

---
+++ form 3/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace form_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(71, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 24);
+            label1.TabIndex = 0;
+            label1.Text = "NOME";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(359, 39);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 24);
+            label2.TabIndex = 1;
+            label2.Text = "CPF";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(619, 40);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 24);
+            label3.TabIndex = 2;
+            label3.Text = "RG";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(71, 91);
+            label4.Name = "label4";
+            label4.Size = new Size(132, 24);
+            label4.TabIndex = 3;
+            label4.Text = "ENDERECO";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(472, 91);
+            label5.Name = "label5";
+            label5.Size = new Size(112, 24);
+            label5.TabIndex = 4;
+            label5.Text = "CELULAR";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Black;
+            label6.Location = new Point(854, 92);
+            label6.Name = "label6";
+            label6.Size = new Size(76, 24);
+            label6.TabIndex = 5;
+            label6.Text = "EMAIL";
+            label6.Click += label6_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Black;
+            label7.Location = new Point(71, 153);
+            label7.Name = "label7";
+            label7.Size = new Size(127, 24);
+            label7.TabIndex = 6;
+            label7.Text = "CONVÊNIO";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(151, 40);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(191, 23);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(418, 40);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(166, 23);
+            textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(676, 39);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(190, 23);
+            textBox3.TabIndex = 9;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(209, 92);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(246, 23);
+            textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(590, 91);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(247, 23);
+            textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(936, 93);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(227, 23);
+            textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(224, 153);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(275, 23);
+            textBox7.TabIndex = 13;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(224, 236);
+            button1.Name = "button1";
+            button1.Size = new Size(160, 41);
+            button1.TabIndex = 14;
+            button1.Text = "Cadastrar Cliente";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(1032, 236);
+            button2.Name = "button2";
+            button2.Size = new Size(161, 43);
+            button2.TabIndex = 15;
+            button2.Text = "Agendar Consulta";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightGreen;
+            ClientSize = new Size(1493, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            ForeColor = Color.Coral;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ form 3/Form1.cs
@@ -0,0 +1,25 @@
+namespace form_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
